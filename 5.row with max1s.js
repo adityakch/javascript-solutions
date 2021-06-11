425ae12fa24a4e3e19efbcf959077d2727f9ede5
adityakch <83400509+adityakch@users.noreply.github.com>
2021-06-11 21:47:00 +0530

    Update 5.row with max1s.js

--- 5.row with max1s.js
+++ 5.row with max1s.js
@@ -3,21 +3,22 @@ function requiredRow(matrix) {
     //console.log(a);
     b = matrix[0].length;
     //console.log(b);
-    max_row_ind = 0;
+    c = 0;
     ind = b-1;
     
     for (let i=0; i<a; i++) {
         if ((ind >= 0) && (matrix[i][ind] == 1)) {
             ind -= 1;
-            max_row_ind = i;
+            c = i;
         }
     }
-    if ((max_row_ind == 0) && (matrix[0][b-1] == 0)) {
+    if ((c == 0) && (matrix[0][b-1] == 0)) {
         return 0;
     }
-    return max_row_ind;
+    return c;
 }
 
 
 let matrix  = [[0,0,0,1],[1,0,1,1],[1,1,1,1],[0,0,0,0]];
-console.log(requiredRow(matrix));
+let res = requiredRow(matrix);
+console.log(res);
